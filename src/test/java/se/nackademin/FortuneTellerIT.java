1cdb586b6d7912872120551e623255c894e2bc09
Farzaneh Yosefi <farzaneh.yosefi@lm.se>
2017-04-10 20:51:50 +0200

    kompletering i testfall i FortuneTellerIT

--- src/test/java/se/nackademin/FortuneTellerIT.java
+++ src/test/java/se/nackademin/FortuneTellerIT.java
@@ -1,6 +1,10 @@
 package se.nackademin;
 
+import java.util.function.Predicate;
 import static junit.framework.Assert.assertFalse;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,22 +25,6 @@ public class FortuneTellerIT {
         assertEquals("Fortune should be calculated correctly", "Din framtid är mjuk. Du borde sluta se. Vi ser att du snart kommer att skaffa en fotboja. Snart kommer du vilja mäta, men då är det viktigt att du är mörk.", output);
     }
 
-    //Issue #37:  Svante/-10000/Malmö/27/0 ger ArrayIndexOutOfBoundsException
-    //Löst genom att ignorera tecken på inkomst (ex. -10000 = 10000)
-    @Test
-    public void testNegIncomeToPosIncom() {
-        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
-        fortuneTeller.setAge("-27");
-        fortuneTeller.setHeight("-165");
-        fortuneTeller.setIncome("-10000");
-        fortuneTeller.setLocation("Malmö");
-        fortuneTeller.setName("Svante");
-        String output = fortuneTeller.calculate();
-        assertEquals("Verify issue #37 have been fixed, negative income input should switch to positive value.", "Din framtid är vacker. Du borde sluta resa. Vi ser att du snart kommer att skaffa en lönesänkning. Snart kommer du vilja mäta, men då är det viktigt att du är snabb.", output);
-
-    }
-    //incomeValue <= 10_000_000
-
     @Test
     public void testIncomeLessThen_10_000_000() {
         FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
@@ -52,8 +40,8 @@ public class FortuneTellerIT {
         assertTrue("Should return true", output);
 
     }
-    
-        @Test
+
+    @Test
     public void testIncomeGreterThen_10_000_000() {
         FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
         boolean output = fortuneTeller.setIncome("10000001");
@@ -107,32 +95,68 @@ public class FortuneTellerIT {
         assertFalse("should return false,", fortuneTeller.setLocation(""));
     }
 
+    //Issue #37:  Svante/-10000/Malmö/27/0 ger ArrayIndexOutOfBoundsException
+    //Löst genom att ignorera tecken på inkomst (ex. -10000 = 10000)
+    @Test
+    public void testNegIncomeToPosIncom() {
+        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+
+        fortuneTeller.setAge("-27");
+        fortuneTeller.setHeight("-165");
+        fortuneTeller.setIncome("-10000");
+        fortuneTeller.setLocation("Malmö");
+        fortuneTeller.setName("Svante");
+        String actual = fortuneTeller.calculate();
+
+        fortuneTeller.setIncome("10000");
+        String expected = fortuneTeller.calculate();
+
+        assertThat(actual, is(equalTo(expected)));
+
+    }
+
     //Issue #38: Svante/10000/Malmö/27/0 ger ArrayIndexOutOfBoundsException
     //Löst genom att tvinga C att vara negativ innan man kommer in i while-loopen.
     @Test
     public void testForstCtobeNegativ() {
-        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        MagicNumbers magicNumbers = new MagicNumbers();
+        Translator translator = new Translator();
+
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbers, translator);
         fortuneTeller.setAge("27");
         fortuneTeller.setHeight("165");
         fortuneTeller.setIncome("10000");
         fortuneTeller.setName("Svante");
         fortuneTeller.setLocation("Malmö");
-        String output = fortuneTeller.calculate();
-        assertEquals("Verify issue #38 is fixed, ", "Din framtid är vacker. Du borde sluta resa. Vi ser att du snart kommer att skaffa en lönesänkning. Snart kommer du vilja mäta, men då är det viktigt att du är snabb.", output);
+
+        String actual = fortuneTeller.calculate();
+
+        int AB = magicNumbers.calculateA() + magicNumbers.calculateB();
+        int c = -Math.abs(AB);
+
+        String expected = fortuneTeller.calculate();
+
+        assertThat(actual, is(equalTo(expected)));
+
     }
 
     //Issue #39: Svante/10000/Malmö/-5/165 ger ArrayIndexOutOfBoundsException
     //Löst genom att ignorera tecken på ålder (ex. -27 = 27)
     @Test
     public void testDenyMinusAge() {
+
         FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
         fortuneTeller.setAge("-27");
         fortuneTeller.setHeight("165");
         fortuneTeller.setIncome("10000");
         fortuneTeller.setName("Svante");
         fortuneTeller.setLocation("Malmö");
-        String output = fortuneTeller.calculate();
-        assertEquals("Verify issue #39 is fixed, ", "Din framtid är vacker. Du borde sluta resa. Vi ser att du snart kommer att skaffa en lönesänkning. Snart kommer du vilja mäta, men då är det viktigt att du är snabb.", output);
+        String actual = fortuneTeller.calculate();
+
+        fortuneTeller.setAge("27");
+        String expected = fortuneTeller.calculate();
+
+        assertThat(actual, is(equalTo(expected)));
 
     }
 
@@ -140,14 +164,20 @@ public class FortuneTellerIT {
     //Löst genom att ignorera tecken på längd (ex. -165 = 165);
     @Test
     public void testDenyMinusHeight() {
+
         FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
         fortuneTeller.setAge("27");
         fortuneTeller.setHeight("-165");
         fortuneTeller.setIncome("10000");
         fortuneTeller.setName("Svante");
         fortuneTeller.setLocation("Malmö");
-        String output = fortuneTeller.calculate();
-        assertEquals("Verify issue #40 is fixed, ", "Din framtid är vacker. Du borde sluta resa. Vi ser att du snart kommer att skaffa en lönesänkning. Snart kommer du vilja mäta, men då är det viktigt att du är snabb.", output);
+
+        String actual = fortuneTeller.calculate();
+
+        fortuneTeller.setHeight("165");
+        String expected = fortuneTeller.calculate();
+
+        assertThat(actual, is(equalTo(expected)));
 
     }
     //Issue #41: Svante/0/Malmö/27/165 ger ArrayIndexOutOfBoundsException
@@ -155,14 +185,24 @@ public class FortuneTellerIT {
 
     @Test
     public void testForstDtobNegativ() {
-        FortuneTeller fortuneTeller = new FortuneTeller(new MagicNumbers(), new Translator());
+        
+        MagicNumbers magicNumbers = new MagicNumbers();
+        Translator translator = new Translator();
+        
+        FortuneTeller fortuneTeller = new FortuneTeller(magicNumbers,  translator);
         fortuneTeller.setAge("27");
         fortuneTeller.setHeight("165");
         fortuneTeller.setIncome("0");
         fortuneTeller.setName("Svante");
         fortuneTeller.setLocation("Malmö");
-        String output = fortuneTeller.calculate();
-        assertEquals("Verify issue #41 is fixed, ", "Din framtid är vacker. Du borde sluta äta. Vi ser att du snart kommer att skaffa ett elstängsel. Snart kommer du vilja röra, men då är det viktigt att du är stor.", output);
+        String actual = fortuneTeller.calculate();
+        
+        int D = -Math.abs(magicNumbers.calculateD());        
+        String expected = fortuneTeller.calculate();
+        
+        assertThat(actual, is(equalTo(expected)));
+        
+      //  assertEquals("Verify issue #41 is fixed, ", "Din framtid är vacker. Du borde sluta äta. Vi ser att du snart kommer att skaffa ett elstängsel. Snart kommer du vilja röra, men då är det viktigt att du är stor.", output);
 
     }
 
